0aab04c10005c0cd7b370d682009b009bb60486a
Isaac Elliott <isaace71295@gmail.com>
2023-08-25 10:17:30 +1000

    clean up ANF translation

--- src/Metis/Anf.hs
+++ src/Metis/Anf.hs
@@ -17,14 +17,15 @@ module Metis.Anf (
   AnfBuilderT,
   runAnfBuilderT,
   freshVar,
+  emit,
   letS,
   letC,
 ) where
 
 import Bound.Scope.Simple (fromScope)
 import Bound.Var (unvar)
-import Control.Monad.State.Class (get, put)
-import Control.Monad.State.Strict (StateT, evalStateT)
+import Control.Monad.State.Class (gets, modify)
+import Control.Monad.State.Strict (StateT, runStateT)
 import Data.Functor.Identity (runIdentity)
 import Data.Hashable (Hashable)
 import Data.Word (Word64)
@@ -51,11 +52,15 @@ data Compound
 data Binop = Add | Subtract
   deriving (Show, Eq)
 
-newtype AnfBuilderT m a = AnfBuilderT {value :: StateT Word64 m a}
+data AnfBuilderState = AnfBuilderState {nextVar :: Word64, program :: Expr -> Expr}
+
+newtype AnfBuilderT m a = AnfBuilderT {value :: StateT AnfBuilderState m a}
   deriving (Functor, Applicative, Monad)
 
-runAnfBuilderT :: (Monad m) => AnfBuilderT m a -> m a
-runAnfBuilderT ma = evalStateT ma.value 0
+runAnfBuilderT :: (Monad m) => AnfBuilderT m Simple -> m Expr
+runAnfBuilderT ma = do
+  (a, s) <- runStateT ma.value AnfBuilderState{nextVar = 0, program = id}
+  pure $ s.program (Simple a)
 
 newtype Var = MkVar Word64
   deriving (Show, Eq, Hashable)
@@ -63,19 +68,12 @@ newtype Var = MkVar Word64
 freshVar :: (Monad m) => AnfBuilderT m Var
 freshVar =
   AnfBuilderT $ do
-    n <- get
-    put $ n + 1
+    n <- gets (.nextVar)
+    modify (\s -> s{nextVar = n + 1})
     pure $ MkVar n
 
-letS :: (Monad m) => VarInfo -> Simple -> (Var -> AnfBuilderT m Expr) -> AnfBuilderT m Expr
-letS varInfo value mkRest = do
-  var <- freshVar
-  LetS var varInfo value <$> mkRest var
-
-letC :: (Monad m) => VarInfo -> Compound -> (Var -> AnfBuilderT m Expr) -> AnfBuilderT m Expr
-letC varInfo value mkRest = do
-  var <- freshVar
-  LetC var varInfo value <$> mkRest var
+emit :: (Monad m) => (Expr -> Expr) -> AnfBuilderT m ()
+emit f = AnfBuilderT $ modify (\s -> s{program = s.program . f})
 
 data VarInfo = VarInfo {size :: Word64}
   deriving (Show, Eq)
@@ -83,104 +81,38 @@ data VarInfo = VarInfo {size :: Word64}
 typeVarInfo :: Type -> VarInfo
 typeVarInfo ty = VarInfo{size = Type.sizeOf ty}
 
-fromCore :: (a -> (Var, Type)) -> Core.Expr a -> Expr
-fromCore toVar expr =
-  runIdentity . runAnfBuilderT $ do
-    expr' <- fromCoreExpr toVar expr
-    resultToExpr (Core.typeOf (snd . toVar) expr) expr'
+letS :: (Monad m) => Type -> Simple -> AnfBuilderT m Var
+letS ty value = do
+  var <- freshVar
+  emit $ LetS var (typeVarInfo ty) value
+  pure var
 
-splitExpr :: Expr -> (Expr -> Expr, Simple)
-splitExpr expr =
-  case expr of
-    Simple simple -> (id, simple)
-    LetS var info simple rest ->
-      let (f, x) = splitExpr rest
-       in (LetS var info simple . f, x)
-    LetC var info compound rest ->
-      let (f, x) = splitExpr rest
-       in (LetC var info compound . f, x)
+letC :: (Monad m) => Type -> Compound -> AnfBuilderT m Var
+letC ty value = do
+  var <- freshVar
+  emit $ LetC var (typeVarInfo ty) value
+  pure var
 
-data FromCoreResult
-  = SimpleResult Simple
-  | CompoundResult Compound
-  | ExprResult Expr
+fromCore :: (a -> Var) -> Core.Expr a -> Expr
+fromCore toVar =
+  runIdentity . runAnfBuilderT . fromCoreExpr toVar
 
-resultToExpr ::
-  (Monad m) =>
-  Type ->
-  FromCoreResult ->
-  AnfBuilderT m Expr
-resultToExpr ty result =
-  case result of
-    SimpleResult simple -> letS (typeVarInfo ty) simple (pure . Simple . Var)
-    CompoundResult compound -> letC (typeVarInfo ty) compound (pure . Simple . Var)
-    ExprResult expr -> pure expr
-
-letResult :: (Monad m) => FromCoreResult -> Type -> (Var -> AnfBuilderT m Expr) -> AnfBuilderT m Expr
-letResult value ty rest =
-  case value of
-    SimpleResult valueSimple ->
-      letS (typeVarInfo ty) valueSimple rest
-    CompoundResult valueCompound ->
-      letC (typeVarInfo ty) valueCompound rest
-    ExprResult valueExpr -> do
-      let (valueExprInit, valueExprLast) = splitExpr valueExpr
-      valueExprInit <$> letS (typeVarInfo ty) valueExprLast rest
-
-fromCoreExpr :: (Monad m) => (a -> (Var, Type)) -> Core.Expr a -> AnfBuilderT m FromCoreResult
+fromCoreExpr :: (Monad m) => (a -> Var) -> Core.Expr a -> AnfBuilderT m Simple
 fromCoreExpr toVar expr =
   case expr of
-    Core.Var var -> pure . SimpleResult $ Var (fst $ toVar var)
-    Core.Literal lit -> pure . SimpleResult $ Literal lit
+    Core.Var var ->
+      pure $ Var (toVar var)
+    Core.Literal lit ->
+      pure $ Literal lit
     Core.Add ty a b -> do
       a' <- fromCoreExpr toVar a
-      ExprResult
-        <$> letResult
-          a'
-          (Core.typeOf (snd . toVar) a)
-          ( \aVar -> do
-              b' <- fromCoreExpr toVar b
-              letResult
-                b'
-                (Core.typeOf (snd . toVar) b)
-                ( \bVar -> do
-                    letC (typeVarInfo ty) (Binop Add (Var aVar) (Var bVar)) (pure . Simple . Var)
-                )
-          )
+      b' <- fromCoreExpr toVar b
+      Var <$> letC ty (Binop Add a' b')
     Core.Subtract ty a b -> do
       a' <- fromCoreExpr toVar a
-      ExprResult
-        <$> letResult
-          a'
-          (Core.typeOf (snd . toVar) a)
-          ( \aVar -> do
-              b' <- fromCoreExpr toVar b
-              letResult
-                b'
-                (Core.typeOf (snd . toVar) b)
-                ( \bVar -> do
-                    letC (typeVarInfo ty) (Binop Subtract (Var aVar) (Var bVar)) (pure . Simple . Var)
-                )
-          )
-    Core.Let ty _ valueTy value rest -> do
+      b' <- fromCoreExpr toVar b
+      Var <$> letC ty (Binop Subtract a' b')
+    Core.Let _ _ valueTy value rest -> do
       value' <- fromCoreExpr toVar value
-      case value' of
-        SimpleResult valueSimple ->
-          ExprResult
-            <$> letS
-              (typeVarInfo valueTy)
-              valueSimple
-              (\var -> fromCoreExpr (unvar (\() -> (var, valueTy)) toVar) (fromScope rest) >>= resultToExpr ty)
-        CompoundResult valueCompound ->
-          ExprResult
-            <$> letC
-              (typeVarInfo valueTy)
-              valueCompound
-              (\var -> fromCoreExpr (unvar (\() -> (var, valueTy)) toVar) (fromScope rest) >>= resultToExpr ty)
-        ExprResult valueExpr -> do
-          let (valueExprInit, valueExprLast) = splitExpr valueExpr
-          ExprResult . valueExprInit
-            <$> letS
-              (typeVarInfo valueTy)
-              valueExprLast
-              (\var -> fromCoreExpr (unvar (\() -> (var, valueTy)) toVar) (fromScope rest) >>= resultToExpr ty)
+      var <- letS valueTy value'
+      fromCoreExpr (unvar (\() -> var) toVar) (fromScope rest)
